7676a7c7bdc441294802316caa4410ddce4e376a
Gaurav Singh <30380576+gauravsinghigc@users.noreply.github.com>
2025-02-25 12:57:15 +0530

    Brochure and ui updated
    Brochure and UI updated!

--- includes/FooterComponents/FlashPopUpWindow.php
+++ includes/FooterComponents/FlashPopUpWindow.php
@@ -6,11 +6,16 @@
     $DisplayPopUpForm = 7000;
 } ?>
 <section class="screen-container" style="display: <?php echo $HideForm; ?>;" id="InstantConsultantForm">
+    <a onclick="HidePopUpForm()" style="position: fixed;top: 3rem;right: 5rem;color: #f60000;font-size: 2rem !important;" class="gsi-btn btn-sm fs-6"><i class="fa fa-times"></i></a>
     <div class="content-box">
         <div class="container gsi-blog-area bg-gradient">
             <div class="row">
-                <div class="col-md-12 pt-5 pb-5">
-                    <h4 class="text-white">Book Free Consultant Now</h4>
+                <div class="col-md-12">
+                    <center>
+                        <img src="<?php echo APP['LOGO']['SQAURE']; ?>" style="margin:1% auto; width:7%;">
+                    </center>
+                    <h4 class="text-white text-center pb-1 mb-0">Book Free Consultant Now</h4>
+                    <p class="text-white small fs-5 mt-0 text-center">Fill the form below and we'll reach you within 24 hours. We'll be happy to help you.</p>
                     <hr>
                     <form class="row" method="POST" action="<?php echo DOMAIN; ?>/controller/InstantBookFreeConsultant.php">
                         <div class="form-group col-md-6 col-sm-6 col-6">
@@ -29,11 +34,6 @@
                             <button type="submit" name="BOOK_FREE_CONSULTANT_INSTANT" class="gsi-btn btn-block text-white">Book Free Consultant <i class="fa fa-angle-right"></i></button>
                         </div>
                     </form>
-                </div>
-
-                <div class="col-md-12 text-center">
-                    <hr>
-                    <a onclick="HidePopUpForm()" style="position: fixed;top: 5rem;right: 5rem;color: #f60000;font-size: 2rem !important;" class="gsi-btn btn-sm fs-6"><i class="fa fa-times"></i></a>
                 </div>
             </div>
         </div>
